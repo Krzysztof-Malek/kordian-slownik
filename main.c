a244af0be1728aa0a92d94de535121470102cd84
Krzysztof Małek <krzymal450@student.polsl.pl>
2021-01-15 20:36:03 +0100

    Wersja beta 1.0

--- main.c
+++ main.c
@@ -16,6 +16,12 @@ Projekt PK2 - Gra w statki
 #include "structs.h"
 
 int main() {
+	/*int n, i, naj;
+	printf("Podaj n: ");
+	scanf("%d", &n);
+
+	int* a = new int[n];*/
+
 	int m = 6;//ustalasz m i n (na razie tak, potem mo¿e bardziej interaktywnie)
 	int n = 3;
 
@@ -28,45 +34,64 @@ int main() {
 		exit(1);
 	}
 
-	char slowo[] = "";	//zmienne slowo i koniec do zapisywania tymczasowych slów z alfabetu
-	char koniec[] = "";
+	char* slowo = malloc(1 * sizeof(char));	//zmienne slowo i koniec do zapisywania tymczasowych slów z alfabetu
+	char* koniec = malloc(1 * sizeof(char));
+
+	//char koniec[] = "";
 
 	fpos_t pozycja = 0;	//zmienna do zapisanie pozycji pliku aby wróciæ do miejsca gdzie skoñczyliœmy (pobieranie kolejnego s³owa z kolejki)
 
 	while (fscanf(slownik, "%s", slowo)) {	//pobieranie kolejnego s³owa z kolejki s³ownika
-		printf("%s \n", slowo); //tymczasowe wyœwietlanie
+		if (feof(slownik) != 0) return 0;	//koñczenie programu jeœli doszliœmy do koñca pliku
+		//printf("%s \n", slowo); //tymczasowe wyœwietlanie
 
 		if (strlen(slowo) == m) {	//je¿eli s³owo mniejsze ni¿ m to nie bierzemy pod uwagê
 			fgetpos(slownik, &pozycja);	//zapisywanie pozycji w której skoñczyliœmy, aby nie zaczynaæ poenieraæ s³ów z pocz¹tku s³ownika (np. skoñczyliœmy na arbuz wiêc zaczniemy na kolejnym a nie 5 s³ów dalej)
 			rewind(slownik);	//powrót do pocz¹tku pliku aby móc porównaæ s³owo (jego koñcówkê) ze wszystkimi s³owami ze s³ownika
 			int a = 0;	//zmienna warunkowa, która okreœla czy koñcówka s³owa wyst¹pi³a w s³owniku
 			do {
+				a = 0;
 				fscanf(slownik, "%s", koniec);	//pobranie kolejnej koñcówki do porównania ze s³owem
 				if (feof(slownik) != 0) break;	//sprawdzenia czy nie wyst¹pi³ koniec pliku (czy sprawdziliœmy wszystkie koñcówki)
-				for (int i = n; i < M; i++) {
-					if (slowo[i] != koniec[i - n])break;	//sprawdzenie n ostatnicz liter s³owa w koñcówce (czyli czy powinniœmy wykluczyæ s³owo i go nie wypisaæ)
-					a++;	//je¿eli kolejne litery siê zgadzaj¹ to a++
+				if (strlen(koniec) == n) {
+					for (int i = n; i < M; i++) {
+						if (slowo[i] != koniec[i - n])break;	//sprawdzenie n ostatnich liter s³owa w koñcówce (czyli czy powinniœmy wykluczyæ s³owo i go nie wypisaæ)
+						a++;	//je¿eli kolejne litery siê zgadzaj¹ to a++
+					}
 				}
-				if (a == N)break;	//je¿eli koñcówka zgadza siê z koñcówk¹ s³owa to wykluczamy to s³owo i przerywamy pêtle while
-			} while ((slowo[n] >= koniec[0]));	//je¿eli natrafimy na literê o wiêkszej wartoœci (ASCI) to nie ma sensu sprawdzaæ reszty np. pierwsza litera s³owa to a a wiêc bez sensu sprawdzaæ koñcówki zaczynaj¹ce siê od b w górê (plik posortowany alfabetycznie)
+			} while ((slowo[n] >= koniec[0]) && a != N);	//je¿eli natrafimy na literê o wiêkszej wartoœci (ASCI) to nie ma sensu sprawdzaæ reszty np. pierwsza litera s³owa to a a wiêc bez sensu sprawdzaæ koñcówki zaczynaj¹ce siê od b w górê (plik posortowany alfabetycznie), oraz jeœli zgadza siê koñcówka od przodu to równie¿ przerywamy
+
+			rewind(slownik);
+
 			if (a != N) {	//tutaj warunek jeœli nie znaleziono warunku wykluczaj¹cego s³owa ¿eby go wypisaæ jako rozwi¹zanie
-				printf("Siema");	//tymczasowe
-				//je¿eli nie znalaz³ s³owa od przodu to powinnien szukaæ tego od ty³u(jeszcze nie napisane)
+				do {
+					a = 0;
+					fscanf(slownik, "%s", koniec);	//pobranie kolejnej koñcówki do porównania ze s³owem
+					if (feof(slownik) != 0) break;	//sprawdzenia czy nie wyst¹pi³ koniec pliku (czy sprawdziliœmy wszystkie koñcówki)
+					if (slowo[m] == koniec[n]) {
+						if (strlen(koniec) == n) {
+							for (int i = m - 1; i >= n; i--) {
+								if (slowo[i] != koniec[a])break;	//sprawdzenie n ostatnich liter s³owa w koñcówce (czyli czy powinniœmy wykluczyæ s³owo i go nie wypisaæ)
+								a++;	//je¿eli kolejne litery siê zgadzaj¹ to a++
+							}
+						}
+					}
+					if (a == n) {
+						printf("Rozwiazanie to: ");
+						printf("%s", slowo);
+						printf(",  koncowka: ");
+						printf("%s", koniec);
+						printf(" \n");
+					}
+				} while ((slowo[m - 1] >= koniec[0]) && a != N);
 			}
 			fsetpos(slownik, &pozycja);	//przywrócenie pozycji pliku przed sprawdzniem koñcówki ¿eby pobieraæ s³owo kolejne z kolei a nie to na którym skoñczyliœmy sprawdzanie koñcówki s³owa w tym przypadku
-			a = 0;	//wyzerowanie zmiennej warunkowej
+			a = 0;//wyzerowanie zmiennej warunkowej
 		}
 	}
 
-	//printf("%s \n", koniec);
-
-	//free(koniec);
 	free(slowo);
-
-	/*char* tabS = malloc(sizeof(char));
-	char slowo[] = { "bakata" };
-	tabS = slowo;
-	printf("%s", tabS);*/
+	free(koniec);
 
 	fclose(slownik);
 
